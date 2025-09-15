1e0a7e6a74fba07d11f1db103f1568a5acecd5a0
Erdem Pülat <erdem.pulat@ug.bilkent.edu.tr>
2025-09-16 01:11:22 +0300

    ok chmod was the catch

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -1,11 +1,2 @@
-#
-//  ci_pre_xcodebuild.sh
-//  PageStreak
-//
-//  Created by Erdem Pülat on 16.09.2025.
-//
-#!/bin/sh
-
-
-# Install dependencies you manage with CocoaPods.
+#!/bin/bash
 pod install
